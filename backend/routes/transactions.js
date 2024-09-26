5e6f08c9cac7686085c3e9ba0994551985adecb7
RITIK SHARMA <ritiksharma555598@gmail.com>
2024-09-26 20:41:34 +0530

    adding files and code upto for expenses and finish backend code

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,3 +1,4 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense')
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income')
 
 
@@ -11,5 +12,10 @@ router.post('/add-income', addIncome)
 // adding the nedpoint for get the datas
         .get('/get-incomes', getIncomes)
         .delete('/delete-income/:id', deleteIncome)
+
+        // this below are the for exprense
+        .post('/add-expense', addExpense)
+        .get('/get-expenses', getExpense)
+        .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router;
