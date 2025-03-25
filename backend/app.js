c152f836616cec0aed440c3ba51201dc70fc80c6
ritik-02806 <rs_td@vignan.ac.in>
2025-03-25 08:56:03 +0530

    adding gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { db } = require('./db/db');
-const cors = require('cors');
 
 const app = express();
 app.use(express.json());
