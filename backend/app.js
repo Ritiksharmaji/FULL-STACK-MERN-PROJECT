c08b3c71236fd33ea05456d2a2d2dd3d54549514
ritik-02806 <rs_td@vignan.ac.in>
2025-03-27 11:34:34 +0530

    nice

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({
-    origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: '*', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+// }));
 
 
 app.use('/api/auth', require('./routes/authRoutes'));
