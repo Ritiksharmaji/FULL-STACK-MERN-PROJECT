6f7993367e837009843104cfca1ffbbbbf171e0c
RITIK SHARMA <ritiksharma555598@gmail.com>
2025-03-31 14:34:31 +0530

    changing the code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,21 @@ import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
- import Dashboard from "./Components/Dashboard/Dashboard";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Income from "./Components/Income/Income";
 import Expenses from "./Components/Expenses/Expenses";
 import { useGlobalContext } from "./context/globalContext";
 import TransactionHistoryAll from "./History/Transactionall";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import LoginForm from "./Pages/LoginForm";
 import RegisterForm from "./Pages/RegisterForm";
 import { AuthProvider } from "./context/AuthContext";
 import ForgotPassword from "./Pages/ForgetPasswordForm";
-import { useNavigate } from "react-router-dom";
 import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
-  const [active, setActive] = useState(1); // Default to Dashboard
-  const { user } = useGlobalContext(); // Check if user is logged in
-
-  // Function to display data based on selected menu
-
+  const [active, setActive] = useState(1);
+  const { user } = useGlobalContext(); 
 
   const orbMemo = useMemo(() => <Orb />, []);
 
@@ -30,30 +26,32 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginForm />} />
+        
+          <Route path="/" element={<Navigate to="/login" />} />
+          
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          {/* <Route path="" element={<Dashboard />} /> */}
 
           <Route path="/auth" element={<ProtectedRoute />}>
-    <Route
-      path="dash-board"
-      element={
-        <AppStyled bg={bg}>
-          <MainLayout>
-            <Navigation active={active} setActive={setActive} />
-            <main>
-              {active === 1 && <Dashboard />}
-              {active === 2 && < TransactionHistoryAll/>}
-              {active === 3 && < Income/>}
-              {active === 4 && <Expenses />}
-            </main>
-            {orbMemo}
-          </MainLayout>
-        </AppStyled>
-      }
-    />
-  </Route>
+            <Route
+              path="dash-board"
+              element={
+                <AppStyled bg={bg}>
+                  <MainLayout>
+                    <Navigation active={active} setActive={setActive} />
+                    <main>
+                      {active === 1 && <Dashboard />}
+                      {active === 2 && <TransactionHistoryAll />}
+                      {active === 3 && <Income />}
+                      {active === 4 && <Expenses />}
+                    </main>
+                    {orbMemo}
+                  </MainLayout>
+                </AppStyled>
+              }
+            />
+          </Route>
         </Routes>
       </Router>
     </AuthProvider>
